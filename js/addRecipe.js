0aa845e4dfab3a19ae8ac1e98bda6ccf66f06a07
Stephen Perez <perezstephenmathew360@gmail.com>
2024-01-15 18:44:33 +0800

    Add Recipe with JS functions pt.2

--- js/addRecipe.js
+++ js/addRecipe.js
@@ -36,6 +36,33 @@ function addIngredient() {
     container.appendChild(document.createElement('br'));
 }
 
+function addInstruction() {
+    const container = document.querySelector('.instructionContainer');
+
+    // new div for each step
+    const newStepDiv = document.createElement('div');
+
+    // new label
+    const newLabel = document.createElement('span');
+    newLabel.style.fontSize = '18px';
+    newLabel.innerHTML = 'Step ' + (container.children.length + 1) + ':';
+
+    // new input
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = 'newInputs[]';
+    newInput.placeholder = 'Add Instruction';
+
+    // append label and input to new div
+    newStepDiv.appendChild(newLabel);
+    newStepDiv.appendChild(newInput);
+
+    // append new div to container
+    container.appendChild(newStepDiv);
+}
+
+
+
 function browseButton() {
     document.getElementById('browseButton').addEventListener('click', function () {
             document.getElementById('addImage').click();
